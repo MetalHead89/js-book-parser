dd7f365492932b7bf1650646878817c86d3639e4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-12 08:20:16 +0700

    Добавлен React

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return (
+    <div className="app">
+      <ListsContainerContainer />
+    </div>
+  );
+};
+
+export default App;
