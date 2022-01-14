fa95a1b740f30f992c64495900292f1a9b45edea
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-14 10:03:13 +0700

    Добавлена обработка ошибок при старте сервера

--- server/src/server/startServer.js
+++ server/src/server/startServer.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import config from './config.js';
 
-const PORT = config.port;
+const PORT = config.port || 5000;
 const server = express();
 
-const startServer = () => {
-  server.get('/start-parsing', (req, res) => {
-    console.log('parsing started');
-  });
+server.get('/start-parsing', (req, res) => {
+  console.log('parsing started');
+});
 
-  server.listen(PORT, () => {
-    console.log(`Express server has been started on http://localhost:${PORT}`);
-  });
+const startServer = () => {
+  try {
+    server.listen(PORT, () => {
+      console.log(`Server has been started on http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.log('Server Error', e.message);
+    process.exit(1); // Выход из процесса nodejs
+  }
 };
 
 export default startServer;
