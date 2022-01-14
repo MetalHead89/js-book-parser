9ede25f7fc9cd503b11ff543fe7d104f6828b509
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-14 08:57:52 +0700

    Добавлен конфиг для сервера

--- server/src/server/startServer.js
+++ server/src/server/startServer.js
@@ -1,15 +1,16 @@
 import express from 'express';
+import config from './config.js';
 
+const PORT = config.port;
 const server = express();
-const port = 5000;
 
 const startServer = () => {
   server.get('/start-parsing', (req, res) => {
     console.log('parsing started');
   });
 
-  server.listen(port, () => {
-    console.log(`Express server has been started on http://localhost:${port}`);
+  server.listen(PORT, () => {
+    console.log(`Express server has been started on http://localhost:${PORT}`);
   });
 };
 
