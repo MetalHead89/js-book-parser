5a94d684fc1a721dfa22f07e9312aa99d742d3fb
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-17 11:52:27 +0700

    Упрощен код авторизации

--- src/server/src/parser/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  siteURL: 'https://urait.ru/',
-  email: 'katya.turuntaeva@gmail.com',
-  password: 'gg6PiL'
-};
-
-export default config;
--- src/server/src/parser/parser.js
+++ src/server/src/parser/parser.js
@@ -20,28 +20,11 @@ async function runParser() {
 }
 
 async function logIn() {
-  await page.goto(config.siteURL, { waitUntil: 'networkidle2' });
-  await page.waitFor(5000);
-
-  const modalWindow = await page.$x('//*[@id="ajax-form-clean"]');
-  if (modalWindow) {
-    const [closeButton] = await page.$x(
-      '//*[@id="ajax-form-clean"]/div/div[2]/a'
-    );
-    closeButton.click();
-  }
-
-  const [logInButton] = await page.$x(
-    '/html/body/header/div/div/div[5]/div[3]'
-  );
-  logInButton.click();
-
-  const [entryButton] = await page.$x('/html/body/div[5]/div/div/div[2]/a[1]');
-  await entryButton.evaluate((button) => button.click());
+  await page.goto(config.logInURL, { waitUntil: 'networkidle2' });
 
   await page.evaluate((email, password) => {
-    const emailInput = document.querySelectorAll('#email');
-    emailInput[1].value = email;
+    const emailInput = document.querySelector('#email');
+    emailInput.value = email;
 
     const passwordInput = document.querySelector('#password');
     passwordInput.value = password;
