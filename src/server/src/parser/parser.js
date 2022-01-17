2ae97dd959e9ae43673ec8481561c94273fdd20c
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-17 14:17:24 +0700

    Добавлена функция закрвающая модальные окна

--- src/server/src/parser/parser.js
+++ src/server/src/parser/parser.js
@@ -12,7 +12,8 @@ async function runParser() {
     });
 
     page = await browser.newPage();
-    logIn();
+    await logIn();
+    await closeModalWindow();
   } catch {
     console.log('Произошла ошибка запуска парсера');
     await browser.close();
@@ -22,16 +23,44 @@ async function runParser() {
 async function logIn() {
   await page.goto(config.logInURL, { waitUntil: 'networkidle2' });
 
-  await page.evaluate((email, password) => {
-    const emailInput = document.querySelector('#email');
-    emailInput.value = email;
+  await page.evaluate(
+    (email, password) => {
+      const emailInput = document.querySelector('#email');
+      emailInput.value = email;
 
-    const passwordInput = document.querySelector('#password');
-    passwordInput.value = password;
+      const passwordInput = document.querySelector('#password');
+      passwordInput.value = password;
 
-    const submitButton = document.querySelector('#login-form > div > div.content-center-form__submit > button');
-    submitButton.click();
-  }, config.email, config.password);
+      const submitButton = document.querySelector(
+        '#login-form > div > div.content-center-form__submit > button'
+      );
+      submitButton.click();
+    },
+    config.email,
+    config.password
+  );
+
+  await page.waitForNavigation();
+}
+
+async function closeModalWindow() {
+  const modalWindow = await page.$x('//*[@id="ajax-form-clean"]');
+  if (modalWindow) {
+    await page.evaluate(() => {
+      const buttonClose = document.querySelector(
+        '#ajax-form-clean > div > div.popup-close-wrapper > a'
+      );
+
+      buttonClose.click();
+    });
+  }
+
+  // if (page.url() === 'https://urait.ru/') {
+  //   await page.goto(
+  //     'https://urait.ru/book/informacionnye-tehnologii-v-marketinge-489042',
+  //     { waitUntil: 'networkidle2' }
+  //   );
+  // }
 }
 
 export { runParser };
