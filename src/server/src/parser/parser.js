66363e6b8384ddaddc537282abde92953a5c30f0
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-01-19 16:45:40 +0700

    Удален лишний код, скорректирована генерация изначального URL страницы

--- src/server/src/parser/parser.js
+++ src/server/src/parser/parser.js
@@ -64,23 +64,7 @@ async function closeModalWindow() {
 }
 
 async function saveBook(url) {
-  await page.goto(
-    'https://urait.ru/viewer/informacionnye-tehnologii-v-marketinge-489042#page/1'
-  );
-
-  // await page.waitForNavigation();
-  //   await page.waitForSelector('#page_5')
-  // await page.evaluate(() => {
-  //   const el = document.querySelector('#page_5')
-  //   el.scrollIntoView();
-  // });
-
-  // await page.$eval(
-  //   '#viewer__bar__pages-scale > input[type=text]',
-  //   (el) => el.click()
-  // );
-
-  // await page.focus(pageInput);
+  await page.goto(`${url}#page/1`);
 
   await page.waitForSelector('#viewer__bar__pages-scale > span:nth-child(3)');
   const pagesCount = await page.$eval(
@@ -95,45 +79,7 @@ async function saveBook(url) {
       const el = document.querySelector(selector);
       el.scrollIntoView();
     }, selector);
-    // console.log(pageNumber);
-    // const selector = `#page_${pageNumber}`;
-    // await page.waitForSelector(selector);
-    // await page.evaluate((pageNumber) => {
-    //   const currentPage = document.getElementById(selector);
-    //   if (currentPage) {
-    //     currentPage.ScrollIntoView();
-    //   }
-    // }, pageNumber + 1);
   }
-
-  // await page.goto('https://urait.ru/viewer/informacionnye-tehnologii-v-marketinge-489042#page/1', { waitUntil: 'networkidle2' });
-  // await page.waitFor('10000');
-  // await page.goto('https://urait.ru/viewer/informacionnye-tehnologii-v-marketinge-489042#page/2', { waitUntil: 'networkidle2' });
-  // await page.waitFor('10000');
-  // await page.goto('#page_3', { waitUntil: 'networkidle2' });
-  // await page.waitForSelector('#page_3');
-  // await page.goto('#page_4', { waitUntil: 'networkidle2' });
-
-  // await page.waitForSelector(`#page_2`);
-  // // await page.$eval(`#page_1`, (e) => {
-  // //   // e.ScrollIntoView();
-  // // });
-  // await page.focus('#page_2');
-
-  // const p = await page.$(`#page_2`);
-  // console.log(p);
-
-  // for await (let pageNumber of [...Array(pagesCount).keys()]) {
-  //   // console.log(pageNumber);
-  //   const selector = `#page_${pageNumber}`;
-  //   await page.waitForSelector(selector);
-  //   await page.evaluate((pageNumber) => {
-  //     const currentPage = document.getElementById(selector);
-  //     if (currentPage) {
-  //       currentPage.ScrollIntoView();
-  //     }
-  //   }, pageNumber + 1);
-  // }
 }
 
 export { runParser };
